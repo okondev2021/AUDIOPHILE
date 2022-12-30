f625c5a253747e2016a0d0fd8739db9f90fdce24
unknown <sojapril16@gmail.com>
2022-12-30 13:07:17 +0100

    sql

--- audiophile/settings.py
+++ audiophile/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$j82)x6_4g*h^cpq41=ek@e8f-n#k9it_ao$awtg)abyiqpljr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*'] 
 
